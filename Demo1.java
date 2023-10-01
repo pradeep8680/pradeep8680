619d59ea00604e830d4a778b6bd3b836fd471a87
pradeep8680 <144304161+pradeep8680@users.noreply.github.com>
2023-09-30 20:18:47 -0700

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,41 @@
+package Num;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		byte SmallNumber=127;
+		
+		short Number=32478;
+		
+		int BigNumber=1234789249;
+		
+		long WholeNumber=2132247489394382913L;
+		
+		String Name="welcome to java";
+		
+		char SetCharactor='j';
+		
+		float SmallDecimal=99.2347444f;
+		
+	    double BigDecimal=234.23478998743212347834723343;
+	   
+	    System.out.println("SmallNumber  ="+SmallNumber);
+	   
+	    System.out.println("Number       ="+Number);
+	    
+	    System.out.println("BigNumber    ="+BigNumber);
+	    
+	    System.out.println("WholeNumber  ="+WholeNumber);
+	    
+	    System.out.println("Name         ="+Name);
+	    
+	    System.out.println("SetCharactor ="+SetCharactor);
+	    
+	    System.out.println("SmallDecimal ="+SmallDecimal);
+	    
+	    System.out.println("BigDecimal   ="+BigDecimal);
+		
+	}
+
+}
+
