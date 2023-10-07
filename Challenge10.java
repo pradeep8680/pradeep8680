0a727da247f81e429212604cc8b9c996877c8849
pradeep8680 <144304161+pradeep8680@users.noreply.github.com>
2023-10-07 08:04:48 +0530

    Update Challenge10.java

--- Challenge10.java
+++ Challenge10.java
@@ -3,8 +3,55 @@ package Num;
 public class Challenge10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+                int a;
+		
+		System.out.println("enter A Week anyone day ");
+		
+		Scanner pk = new Scanner(System.in);
+		a=pk.nextInt();
+		
+		switch (a) {
+		case 1:
+		{
+			System.out.println("Today is sunday ");
+		}
+			break
+		case 2:
+		{
+			System.out.println("Today is monday");
+		}
+			break;	
+		case 3:
+			System.out.println("Today is Tuesday");
+			break;
+		case 4:
+		{
+			System.out.println("Today is  wednesday");
+		}
+			break;
+		case 5:
+		{
+			System.out.println("Today is Thursday");
+		}
+			break;	
+		case 6:
+		{
+			System.out.println("Today is friday");
+	
+		}
+			break;
+		case 7:
+		{
+			System.out.println("Today is satureday ");
+		}
+			break;
+		default:
+			{
+				System.out.println(" a week is do not have a morethen day ");
+				break;
+			}
+						
 	}
 
 }
